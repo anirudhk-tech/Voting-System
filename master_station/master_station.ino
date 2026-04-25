e59a29e8bbb4e352160772037ccdaf66fd473f03
Anirudh Kuppili <anirudhkuppili.dev@gmail.com>
2026-04-25 07:26:21 -0500

    feat(test): buzzer

--- master_station/master_station.ino
+++ master_station/master_station.ino
@@ -1,10 +1,4 @@
-/*
- * Master - Hardware Serial Test
- * Echoes anything received from Station B by blinking the onboard LED (pin 13).
- * Also sends "PING" to Station B every 2 seconds.
- */
-
-const int LED = 13;  // built-in LED
+const int LED = 13;
 
 void setup() {
   Serial.begin(9600);
@@ -12,7 +6,6 @@ void setup() {
 }
 
 void loop() {
-  // Blink LED briefly when data is received
   if (Serial.available()) {
     char c = Serial.read();
     digitalWrite(LED, HIGH);
@@ -20,7 +13,6 @@ void loop() {
     digitalWrite(LED, LOW);
   }
 
-  // Send a ping every 2 seconds
   static unsigned long last = 0;
   if (millis() - last > 2000) {
     last = millis();
--- voting_station/voting_station.ino
+++ voting_station/voting_station.ino
@@ -1,8 +1,21 @@
+const int BUZZER = 5;
+
 void setup() {
   Serial.begin(9600);
+  pinMode(BUZZER, OUTPUT);
 }
 
 void loop() {
-  Serial.println("HELLO");
-  delay(500);
+  static unsigned long last = 0;
+  if (millis() - last > 1000) {
+    last = millis();
+    Serial.println("HELLO_FROM_B");
+  }
+
+  if (Serial.available()) {
+    char c = Serial.read();
+    if (c == '\n' || c == '\r') return;
+    tone(BUZZER, 1500, 100);
+    delay(120);
+  }
 }
