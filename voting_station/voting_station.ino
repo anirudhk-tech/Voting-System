b5708853db5030218e046500ea82c1365780eb07
Anirudh Kuppili <anirudhkuppili.dev@gmail.com>
2026-04-25 07:39:58 -0500

    feat(test): test each board in iso

--- voting_station/voting_station.ino
+++ voting_station/voting_station.ino
@@ -1,21 +1,68 @@
-const int BUZZER = 5;
+/*
+ * Station B - Component Test
+ * Tests: 2 pushbuttons, green status LED, piezo buzzer, photoresistor
+ */
+
+const int BTN_A     = 2;
+const int BTN_B     = 3;
+const int STATUS_LED = 4;
+const int BUZZER    = 5;
+const int PHOTO_PIN = A0;
+
+int lastBtnA = HIGH;
+int lastBtnB = HIGH;
+
+bool stationActive = true;
+unsigned long lockUntil = 0;
 
 void setup() {
   Serial.begin(9600);
+  Serial.println(F("=== Station B Test ==="));
+
+  pinMode(BTN_A, INPUT_PULLUP);
+  pinMode(BTN_B, INPUT_PULLUP);
+  pinMode(STATUS_LED, OUTPUT);
   pinMode(BUZZER, OUTPUT);
+
+  digitalWrite(STATUS_LED, HIGH);
 }
 
 void loop() {
-  static unsigned long last = 0;
-  if (millis() - last > 1000) {
-    last = millis();
-    Serial.println("HELLO_FROM_B");
+  int btnAState = digitalRead(BTN_A);
+  int btnBState = digitalRead(BTN_B);
+  int lightVal  = analogRead(PHOTO_PIN);
+
+  if (stationActive) {
+    if (btnAState == LOW && lastBtnA == HIGH) {
+      Serial.println(F(">>> Button A"));
+      tone(BUZZER, 1000, 150);
+      stationActive = false;
+      digitalWrite(STATUS_LED, LOW);
+      lockUntil = millis() + 3000;
+    }
+    if (btnBState == LOW && lastBtnB == HIGH) {
+      Serial.println(F(">>> Button B"));
+      tone(BUZZER, 1500, 150);
+      stationActive = false;
+      digitalWrite(STATUS_LED, LOW);
+      lockUntil = millis() + 3000;
+    }
+  }
+  lastBtnA = btnAState;
+  lastBtnB = btnBState;
+
+  if (!stationActive && millis() > lockUntil) {
+    stationActive = true;
+    digitalWrite(STATUS_LED, HIGH);
+    Serial.println(F(">>> Reset"));
   }
 
-  if (Serial.available()) {
-    char c = Serial.read();
-    if (c == '\n' || c == '\r') return;
-    tone(BUZZER, 1500, 100);
-    delay(120);
+  static unsigned long lastPrint = 0;
+  if (millis() - lastPrint > 500) {
+    lastPrint = millis();
+    Serial.print(F("Light=")); Serial.print(lightVal);
+    Serial.print(F(" Active=")); Serial.println(stationActive ? "YES" : "NO");
   }
+
+  delay(20);
 }
