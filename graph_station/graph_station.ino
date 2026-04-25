574b1654721194f0dcb0099ca789c7489ecf83df
Anirudh Kuppili <anirudhkuppili.dev@gmail.com>
2026-04-25 09:57:08 -0500

    feat(prod): 4 voting system

--- graph_station/graph_station.ino
+++ graph_station/graph_station.ino
@@ -1,21 +1,24 @@
 /*
- * Station A - Component Test
- * Pins: LEDs=5,4,3,2 | Buttons=8,9 | Photo=A0
+ * Station A - Standard Voter (LED bar graph)
+ * Pins: BtnA=8, BtnB=9, LEDs=5,4,3,2, Photo=A0
+ * Hardware Serial (pin 0/1) talks to master.
  */
 
-const int LED_PINS[4] = {5, 4, 3, 2};
 const int BTN_A = 8;
 const int BTN_B = 9;
+const int LED_PINS[4] = {5, 4, 3, 2};
 const int PHOTO_PIN = A0;
 
+const char STATION_ID = 'A';
+
+bool stationActive = true;
 int lastBtnA = HIGH;
 int lastBtnB = HIGH;
 
-int currentLevel = 0;
-unsigned long lastLevelChange = 0;
-
-unsigned long specialShowUntil = 0;
-char specialPattern = 0;
+int displayedA = 0;
+int displayedB = 0;
+char displayedMode = 'L';
+bool resultsRevealed = true;
 
 void setBar(int level) {
   for (int i = 0; i < 4; i++) {
@@ -23,68 +26,82 @@ void setBar(int level) {
   }
 }
 
-void setAlternating(bool flip) {
-  for (int i = 0; i < 4; i++) {
-    bool on = (i % 2 == 0) ? !flip : flip;
-    digitalWrite(LED_PINS[i], on ? HIGH : LOW);
+void sendVote(char choice) {
+  for (int i = 0; i < 3; i++) {
+    Serial.print("V");
+    Serial.print(STATION_ID);
+    Serial.print(":");
+    Serial.println(choice);
+    delay(30);
   }
 }
 
-void setAllOn() {
-  for (int i = 0; i < 4; i++) digitalWrite(LED_PINS[i], HIGH);
+void parseIncoming(const String& msg) {
+  if (msg.length() == 0) return;
+
+  if (msg.charAt(0) == 'R') {
+    stationActive = true;
+    displayedA = 0;
+    displayedB = 0;
+    resultsRevealed = true;
+    return;
+  }
+
+  if (msg.charAt(0) == 'T') {
+    int firstColon  = msg.indexOf(':');
+    int firstComma  = msg.indexOf(',');
+    int secondComma = msg.indexOf(',', firstComma + 1);
+    if (firstColon < 0 || firstComma < 0 || secondComma < 0) return;
+
+    displayedA = msg.substring(firstColon + 1, firstComma).toInt();
+    displayedB = msg.substring(firstComma + 1, secondComma).toInt();
+    displayedMode = msg.charAt(secondComma + 1);
+    resultsRevealed = true;
+  }
 }
 
 void setup() {
   Serial.begin(9600);
-  Serial.println(F("=== Station A Test ==="));
-  for (int i = 0; i < 4; i++) {
-    pinMode(LED_PINS[i], OUTPUT);
-    digitalWrite(LED_PINS[i], LOW);
-  }
   pinMode(BTN_A, INPUT_PULLUP);
   pinMode(BTN_B, INPUT_PULLUP);
+  for (int i = 0; i < 4; i++) {
+    pinMode(LED_PINS[i], OUTPUT);
+  }
+  setBar(0);
 }
 
 void loop() {
+  if (Serial.available()) {
+    String msg = Serial.readStringUntil('\n');
+    msg.trim();
+    parseIncoming(msg);
+  }
+
   int btnAState = digitalRead(BTN_A);
   int btnBState = digitalRead(BTN_B);
-  int lightVal  = analogRead(PHOTO_PIN);
 
-  if (btnAState == LOW && lastBtnA == HIGH) {
-    Serial.println(F(">>> Button A"));
-    specialPattern = 'A';
-    specialShowUntil = millis() + 800;
+  if (stationActive) {
+    if (btnAState == LOW && lastBtnA == HIGH) {
+      stationActive = false;
+      if (displayedMode == 'S') resultsRevealed = false;
+      sendVote('A');
+    }
+    if (btnBState == LOW && lastBtnB == HIGH) {
+      stationActive = false;
+      if (displayedMode == 'S') resultsRevealed = false;
+      sendVote('B');
+    }
   }
   lastBtnA = btnAState;
-
-  if (btnBState == LOW && lastBtnB == HIGH) {
-    Serial.println(F(">>> Button B"));
-    specialPattern = 'B';
-    specialShowUntil = millis() + 800;
-  }
   lastBtnB = btnBState;
 
-  if (millis() < specialShowUntil) {
-    if (specialPattern == 'A') setAllOn();
-    else {
-      bool flip = (millis() / 150) % 2;
-      setAlternating(flip);
-    }
+  if (resultsRevealed) {
+    int level = max(displayedA, displayedB);
+    if (level > 4) level = 4;
+    setBar(level);
   } else {
-    if (millis() - lastLevelChange > 500) {
-      lastLevelChange = millis();
-      currentLevel = (currentLevel + 1) % 6;
-    }
-    if (currentLevel == 5) setAllOn();
-    else setBar(currentLevel);
-  }
-
-  static unsigned long lastPrint = 0;
-  if (millis() - lastPrint > 500) {
-    lastPrint = millis();
-    Serial.print(F("Light=")); Serial.print(lightVal);
-    Serial.print(F(" BtnA=")); Serial.print(btnAState);
-    Serial.print(F(" BtnB=")); Serial.println(btnBState);
+    bool on = (millis() / 400) % 2;
+    for (int i = 0; i < 4; i++) digitalWrite(LED_PINS[i], on ? HIGH : LOW);
   }
 
   delay(20);
